6b0c72ae88d75eeec53efd659a886eed8f00a484
Jody Ingram <22178885+Jody-Ingram@users.noreply.github.com>
2025-05-21 09:35:00 -0400

    Create botLauncher.bat

---
+++ FoundryAI/botLauncher.bat
@@ -0,0 +1,10 @@
+:: Script  :  botLauncher.bat
+:: Version :  1.0
+:: Date    :  5/20/25
+:: Author  :  Jody Ingram
+:: Pre-reqs: N/A
+:: Notes   :  Batch file to run the specific foundry model specified.
+:: Notes   :  Can also run this as a windows shortcut: cmd /k "foundry model run phi-4"
+
+:: Change model type if needed
+foundry model run phi-4
